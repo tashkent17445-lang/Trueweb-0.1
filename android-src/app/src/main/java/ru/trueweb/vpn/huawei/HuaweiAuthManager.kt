38de863387e99017fb36d2c420bd27d03c7a7014
tashkent17445-lang <tashkent17445@gmail.com>
2026-09-24 18:09:40 +0500

    Add Huawei Account Kit authorization-code helper

---
+++ android-src/app/src/main/java/ru/trueweb/vpn/huawei/HuaweiAuthManager.kt
@@ -0,0 +1,27 @@
+package ru.trueweb.vpn.huawei
+
+import android.app.Activity
+import android.content.Intent
+import com.huawei.hms.support.account.request.AccountAuthParams
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper
+import com.huawei.hms.support.account.service.AccountAuthManager
+
+object HuaweiAuthManager {
+    fun signInIntent(activity: Activity): Intent {
+        val params = AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+            .setAuthorizationCode()
+            .createParams()
+        return AccountAuthManager.getService(activity, params).signInIntent
+    }
+
+    fun parseAuthorizationCode(data: Intent?): Result<String> = runCatching {
+        requireNotNull(data) { "Huawei sign-in returned no data" }
+        val task = AccountAuthManager.parseAuthResultFromIntent(data)
+        if (!task.isSuccessful) {
+            throw task.exception ?: IllegalStateException("Huawei sign-in failed")
+        }
+        val account = task.result ?: error("Huawei account result is empty")
+        account.authorizationCode?.takeIf { it.isNotBlank() }
+            ?: error("Huawei authorization code is empty")
+    }
+}
