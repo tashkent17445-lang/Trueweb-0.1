4a7936892e1406e7809bc98a0373a457fa06d68e
tashkent17445-lang <tashkent17445@gmail.com>
2026-09-24 18:08:40 +0500

    Configure Huawei Account Kit and IAP SDKs

--- android-src/app/build.gradle.kts
+++ android-src/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("com.huawei.agconnect.agcp")
 }
 
 android {
@@ -56,6 +57,9 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.work:work-runtime-ktx:2.11.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+
+    implementation("com.huawei.hms:hwid:6.13.0.300")
+    implementation("com.huawei.hms:iap:6.16.6.305")
 
     val composeBom = platform("androidx.compose:compose-bom:2026.08.00")
     implementation(composeBom)
