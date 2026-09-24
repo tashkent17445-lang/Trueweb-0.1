7e406f1ab16effa5f586ed8799856d577fc1fb28
tashkent17445-lang <tashkent17445@gmail.com>
2026-09-24 18:24:21 +0500

    Use Huawei SDKs without incompatible AGConnect Gradle plugin

--- android-src/build.gradle.kts
+++ android-src/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
     id("com.android.application") version "9.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
-    id("com.huawei.agconnect.agcp") version "1.9.6.300" apply false
 }
