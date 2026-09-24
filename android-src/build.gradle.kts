3b6c58be25f8d3870312352cd8a5100842c60e36
tashkent17445-lang <tashkent17445@gmail.com>
2026-09-24 20:13:37 +0500

    Configure AppGallery Connect Gradle plugin

--- android-src/build.gradle.kts
+++ android-src/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://developer.huawei.com/repo/") }
+    }
+    dependencies {
+        classpath("com.huawei.agconnect:agcp:1.9.6.300")
+    }
+}
+
 plugins {
     id("com.android.application") version "9.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
