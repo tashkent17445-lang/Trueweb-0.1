1d74df7d433927ae81d431b43f69b2e5ea2c577c
tashkent17445-lang <tashkent17445@gmail.com>
2026-09-24 18:08:36 +0500

    Configure Huawei Account Kit and IAP SDKs

--- android-src/build.gradle.kts
+++ android-src/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.application") version "9.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("com.huawei.agconnect.agcp") version "1.9.6.300" apply false
 }
