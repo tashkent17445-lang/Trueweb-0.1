1d5310d55209f107cd912bd8ffcf6a874f917f3f
tashkent17445-lang <tashkent17445@gmail.com>
2026-09-24 20:14:16 +0500

    Show Huawei Account Kit error codes

--- android-src/app/src/main/java/ru/trueweb/vpn/huawei/HuaweiAuthManager.kt
+++ android-src/app/src/main/java/ru/trueweb/vpn/huawei/HuaweiAuthManager.kt
@@ -2,6 +2,7 @@ package ru.trueweb.vpn.huawei
 
 import android.app.Activity
 import android.content.Intent
+import com.huawei.hms.common.ApiException
 import com.huawei.hms.support.account.request.AccountAuthParams
 import com.huawei.hms.support.account.request.AccountAuthParamsHelper
 import com.huawei.hms.support.account.AccountAuthManager
@@ -18,7 +19,16 @@ object HuaweiAuthManager {
         requireNotNull(data) { "Huawei sign-in returned no data" }
         val task = AccountAuthManager.parseAuthResultFromIntent(data)
         if (!task.isSuccessful) {
-            throw task.exception ?: IllegalStateException("Huawei sign-in failed")
+            val cause = task.exception
+            val code = (cause as? ApiException)?.statusCode
+            val detail = buildString {
+                append("Huawei sign-in failed")
+                if (code != null) append(" (code=").append(code).append(")")
+                cause?.message?.takeIf { it.isNotBlank() }?.let {
+                    append(": ").append(it)
+                }
+            }
+            throw IllegalStateException(detail, cause)
         }
         val account = task.result ?: error("Huawei account result is empty")
         account.authorizationCode?.takeIf { it.isNotBlank() }
